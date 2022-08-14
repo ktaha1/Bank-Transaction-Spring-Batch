7df0c4254977bb00dd1dcf7acf1842d352e6a01e
Taha KAMIL <itaha.kamil@gmail.com>
2022-08-14 17:45:16 +0100

    Initial Commit

---
+++ src/main/java/io/bank/transaction/BankSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package io.bank.transaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSpringBatchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/bank/transaction/BankSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bank.transaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
