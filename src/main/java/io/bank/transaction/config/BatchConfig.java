f6fe3c321bea033952a056e47595b60be06d2a66
Taha KAMIL <itaha.kamil@gmail.com>
2022-08-15 23:14:14 +0100

    Add job and step beans

--- src/main/java/io/bank/transaction/config/BatchConfig.java
+++ src/main/java/io/bank/transaction/config/BatchConfig.java
@@ -1,25 +1,87 @@
 package io.bank.transaction.config;
 
 import io.bank.transaction.model.Transaction;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
-import org.springframework.batch.item.ItemProcessor;
-import org.springframework.batch.item.ItemReader;
-import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.LineMapper;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.mapping.DefaultLineMapper;
+import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableBatchProcessing
-public class BatchConfig {
+@Slf4j
+public class BatchConfig extends DefaultBatchConfigurer {
     @Autowired private JobBuilderFactory job;
     @Autowired private StepBuilderFactory step;
-    @Autowired private ItemReader<Transaction> itemReader;
-    @Autowired private ItemProcessor<Transaction, Transaction> itemProcessor;
-    @Autowired private ItemWriter<Transaction> itemWriter;
 
+    @Value("${inputFilePath}") Resource resource;
+    /*@Autowired private ItemReader<Transaction> itemReader;
+    @Autowired private ItemProcessor<Transaction, Transaction> itemProcessor;
+    @Autowired private ItemWriter<Transaction> itemWriter;*/
+
+    @Override
+    public void setDataSource(DataSource dataSource) {
+        // override to do not set datasource even if a datasource exist.
+        // initialize will use a Map based JobRepository (instead of database)
+    }
+
+    @Bean
+    public FlatFileItemReader<Transaction> reader(){
+        log.info("Writing transactions ....");
+        return new FlatFileItemReaderBuilder<Transaction>()
+                .name("Transaction-Reader")
+                .resource(resource)
+                .delimited()
+                .names("id", "fullName", "date", "type", "amount")
+                .fieldSetMapper(new BeanWrapperFieldSetMapper<Transaction>() {{
+                    setTargetType(Transaction.class);
+                }})
+                .build();
+    }
+
+    @Bean
+    public ItemTransactionProcessor process(){
+        return new ItemTransactionProcessor();
+    }
+
+    @Bean
+    public ItemTransactionWriter writer(){
+        return new ItemTransactionWriter();
+    }
+
+    @Bean
+    public Step stepOne(){
+        log.info("Step Executing ...");
+        return this.step.get("stepOne")
+                .<Transaction, Transaction>chunk(10)
+                .reader(reader())
+                .processor(process())
+                .writer(writer())
+                .build();
+    }
+
+    @Bean
+    public Job executeJob(){
+        log.info("Job Executing ...");
+        return job.get("mainJob")
+                .start(stepOne())
+                .build();
+    }
 
 
 }
--- src/main/java/io/bank/transaction/config/ItemTransactionProcessor.java
+++ src/main/java/io/bank/transaction/config/ItemTransactionProcessor.java
@@ -1,6 +1,7 @@
 package io.bank.transaction.config;
 
 import io.bank.transaction.model.Transaction;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.stereotype.Component;
 
@@ -8,9 +9,11 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 @Component
+@Slf4j
 public class ItemTransactionProcessor implements ItemProcessor<Transaction, Transaction> {
     @Override
-    public Transaction process(Transaction transaction) {
+    public Transaction process(Transaction transaction) throws Exception{
+        log.info("Processing transactions ....");
         String strDate = transaction.getStrDate();
         transaction.setDate(LocalDateTime.parse(strDate, DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss")));
         return transaction;
